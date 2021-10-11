1c08e6a1efdb6b9bce414e4b7a62dd858e5765f3
James Maroney <james.maroney@gmail.com>
2021-10-11 00:02:28 -0400

    updating to save pages in a different way

--- pagediff.py
+++ pagediff.py
@@ -18,23 +18,19 @@ cleaner = Cleaner()
 cleaner.javascript = True  # This is True because we want to activate the javascript filter
 cleaner.style = True  # This is True because we want to activate the styles & stylesheet filter
 
-def get_url_dir(url):
-    return os.path.join(PAGES_DIR, quote_plus(url))
+def get_capture_path(url):
+    return os.path.join(PAGES_DIR, f'{quote_plus(url)}.html')
 
-def ensure_url_dir(url):
-  dirname = get_url_dir(url)
-  if not os.path.isdir(dirname):
+def ensure_pages_dir():
+  if not os.path.isdir(PAGES_DIR):
       print(f'  creating dir {dirname}')
-      os.makedirs(dirname, exist_ok=True)
+      os.makedirs(PAGES_DIR, exist_ok=True)
 
 def get_last_url_state(url):
-    dirname = get_url_dir(url)
-    captures = glob.glob(os.path.join(dirname, '*.html'))
-    if not captures: return b''
-
-    latest_capture = max(captures, key=os.path.getctime)
-    print(f'  >> loading latest capture: {latest_capture}')
-    return open(latest_capture, 'rb').read() if latest_capture else b''
+    capture_path = get_capture_path(url)
+    if not os.path.isfile(capture_path): return b''
+    print(f'  >> loading latest capture: {capture_path}')
+    return open(capture_path, 'rb').read()
 
 def get_current_url_state(url):
     # TODO: handle error
@@ -42,8 +38,7 @@ def get_current_url_state(url):
     return f.read()
 
 def write_new_state(url, state):
-    dirname = get_url_dir(url)
-    capture = os.path.join(dirname, f'{NOW}.html')
+    capture = get_capture_path(url)
     print(f'  >> writing new capture: {capture}')
     with open(capture, 'wb') as FOUT:
         FOUT.write(state)
@@ -63,9 +58,9 @@ with open(PAGES_FILE) as file:
 
 for url in urls:
   print(f'Handling :: {url}')
-  ensure_url_dir(url)
+  ensure_pages_dir()
   last_state = get_last_url_state(url)
   current_state = get_current_url_state(url)
-  if states_differ(last_state, current_state):
+  if not last_state or states_differ(last_state, current_state):
       print(f'  >> differences found!')
       write_new_state(url, current_state)
